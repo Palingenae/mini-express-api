a35983da9662568ddda3cbea197fe220c5c46e1f
Olivia D. S <hello@palingenae.be>
2024-02-13 19:19:25 +0100

    INIT: app telle que laissée à la fin de l'entretien avec premier SoC

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const securityController = require('./src/controllers/securityController');
+
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    response.send('Hello there');
+});
+
+app.post("/login", securityController.logIn);
+
+app.listen(port, () => {
+    console.log(`L'application écoute sur le port ${port}`);
+});
